0c7a292501d8c6019907a71c51a808fde4a09b35
K <ksaynice@gmail.com>
2015-04-19 12:25:32 +0800

    rna-transcription passed

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -1,2 +1,26 @@
 class Complement
+  def self.of_dna(nucleotide)
+    nucleotide.each_char.map do |char|
+      raise ArgumentError unless nucleotides_map.include?(char)
+      nucleotides_map[char]
+    end.join('')
+  end
+
+  def self.of_rna(nucleotide)
+    nucleotide.each_char.map do |char|
+      raise ArgumentError unless nucleotides_map.invert.include?(char)
+      nucleotides_map.invert[char]
+    end.join('')
+  end
+
+  private
+
+  def self.nucleotides_map
+    {
+      'G' => 'C',
+      'C' => 'G',
+      'T' => 'A',
+      'A' => 'U'
+    }
+  end
 end
